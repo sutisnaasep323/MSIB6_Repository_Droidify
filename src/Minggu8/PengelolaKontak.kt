21ce853c7c08c4ce60a901344305816df7528ca0
Asep Sutisna <sutisnaasep323@gmail.com>
2024-04-18 21:52:26 +0700

    Update extension function View Contact @Asep Sutisna

--- src/Minggu8/PengelolaKontak.kt
+++ src/Minggu8/PengelolaKontak.kt
@@ -3,84 +3,71 @@ package Minggu8
 import java.util.Scanner
 
 data class Kontak(
-       private val name: String, // Visibility control ditambahkan disini
-       private val phoneNumber: String, // Visibility control ditambahkan disini
-       private val email: String // Visibility control ditambahkan disini
-)
-{ //tambahkam getter method
-        fun getName(): String {
-                return name
-        }
+        private val name: String,
+        private val phoneNumber: String,
+        private val email: String
+) {
+    fun getName(): String {
+        return name
+    }
 
-        fun getPhoneNumber(): String {
-                return phoneNumber
-        }
+    fun getPhoneNumber(): String {
+        return phoneNumber
+    }
 
-        fun getEmail(): String {
-                return email
-        }
+    fun getEmail(): String {
+        return email
+    }
 }
 
-// A sample class to demonstrate extension functions
-class Circle (val radius: Double){
-    // member function of class
-    fun area(): Double{
-        return Math.PI * radius * radius;
+class PengelolaKontak {
+    private val kontakList: MutableList<Kontak> = mutableListOf()
+
+    //Atribut kontakList tetap private karena ini merupakan struktur data internal dari kelas PengelolaKontak dan tidak seharusnya diakses langsung dari luar
+    // untuk (tambahKontak, hapusKontak, tampilkanKontak) metode publik karena memang seharusnya bisa diakses dari luar kelas tersebut.
+    fun tambahKontak(kontak: Kontak) {
+        kontakList.add(kontak)
+        println("Kontak '${kontak.getName()}' berhasil ditambahkan.")
+    }
+
+    fun hapusKontak(namaKontak: String) {
+        val kontakDitemukan = kontakList.find { it.getName() == namaKontak }
+        if (kontakDitemukan != null) {
+            kontakList.remove(kontakDitemukan)
+            println("Kontak '$namaKontak' berhasil dihapus.")
+        } else {
+            println("Kontak '$namaKontak' tidak ditemukan.")
+        }
+    }
+
+    fun Kontak.printContactInfo() {
+        println("Nama: ${getName()}, Nomor Telepon: ${getPhoneNumber()}, Email: ${getEmail()}")
+    }
+
+
+    fun tampilkanKontak() {
+        if (kontakList.isEmpty()) {
+            println("Daftar kontak kosong.")
+        } else {
+            println("Daftar Kontak:")
+            kontakList.forEachIndexed { index, kontak ->
+                print("${index + 1}. ")
+                kontak.printContactInfo()
+            }
+        }
     }
 }
 
 
-class PengelolaKontak {
-        private val kontakList: MutableList<Kontak> = mutableListOf()
-        //Atribut kontakList tetap private karena ini merupakan struktur data internal dari kelas PengelolaKontak dan tidak seharusnya diakses langsung dari luar
-        // untuk (tambahKontak, hapusKontak, tampilkanKontak) metode publik karena memang seharusnya bisa diakses dari luar kelas tersebut.
-        fun tambahKontak(kontak: Kontak) {
-                kontakList.add(kontak)
-                println("Kontak '${kontak.getName()}' berhasil ditambahkan.")
-        }
-
-        fun hapusKontak(namaKontak: String) {
-                val kontakDitemukan = kontakList.find { it.getName() == namaKontak }
-                if (kontakDitemukan != null) {
-                        kontakList.remove(kontakDitemukan)
-                        println("Kontak '$namaKontak' berhasil dihapus.")
-                } else {
-                        println("Kontak '$namaKontak' tidak ditemukan.")
-                }
-        }
-
-        fun tampilkanKontak() {
-                if (kontakList.isEmpty()) {
-                        println("Daftar kontak kosong.")
-                } else {
-                        println("Daftar Kontak:")
-                        kontakList.forEachIndexed { index, kontak ->
-                                println("${index + 1}. ${kontak.getName()}, ${kontak.getPhoneNumber()}, ${kontak.getEmail()}")
-                        }
-                }
-        }
-}
-
-
 fun main() {
-        val scanner = Scanner(System.`in`)
-        val pengelolaKontak = PengelolaKontak()
+    val scanner = Scanner(System.`in`)
+    val pengelolaKontak = PengelolaKontak()
 
-        fun Circle.perimeter(): Double{
-                return 2*Math.PI*radius;
-        }
+    println("Selamat datang di Pengelola Kontak!")
 
-        val newCircle = Circle(2.5);
-        // invoke member function
-        println("Area of the circle is ${newCircle.area()}")
-        // invoke extension function
-        println("Perimeter of the circle is ${newCircle.perimeter()}")
-
-        println("Selamat datang di Pengelola Kontak!")
-
-        var isRunning = true
-        while (isRunning) {
-                println("""
+    var isRunning = true
+    while (isRunning) {
+        println("""
             Pilih operasi yang ingin dilakukan:
             1. Tambah Kontak
             2. Hapus Kontak
@@ -88,34 +75,35 @@ fun main() {
             4. Keluar
         """.trimIndent())
 
-                print("Masukkan pilihan (1/2/3/4): ")
-                when (scanner.nextInt()) {
-                        1 -> {
-                                print("Masukkan nama kontak: ")
-                                val name = scanner.next()
-                                print("Masukkan nomor telepon: ")
-                                val phoneNumber = scanner.next()
-                                print("Masukkan alamat email: ")
-                                val email = scanner.next()
-                                val kontak = Kontak(name, phoneNumber, email)
-                                pengelolaKontak.tambahKontak(kontak)
-                        }
-                        2 -> {
-                                print("Masukkan nama kontak yang ingin dihapus: ")
-                                val namaKontak = scanner.next()
-                                pengelolaKontak.hapusKontak(namaKontak)
-                        }
-                        3 -> {
-                                pengelolaKontak.tampilkanKontak()
-                        }
-                        4 -> {
-                                println("Terima kasih!")
-                                isRunning = false
-                        }
-                        else -> {
-                                println("Pilihan tidak valid. Silakan pilih 1, 2, 3, atau 4.")
-                        }
-                }
+        print("Masukkan pilihan (1/2/3/4): ")
+        when (scanner.nextInt()) {
+            1 -> {
+                print("Masukkan nama kontak: ")
+                val name = scanner.next()
+                print("Masukkan nomor telepon: ")
+                val phoneNumber = scanner.next()
+                print("Masukkan alamat email: ")
+                val email = scanner.next()
+                val kontak = Kontak(name, phoneNumber, email)
+                pengelolaKontak.tambahKontak(kontak)
+            }
+            2 -> {
+                pengelolaKontak.tampilkanKontak()
+                print("Masukkan nama kontak yang ingin dihapus: ")
+                val namaKontak = scanner.next()
+                pengelolaKontak.hapusKontak(namaKontak)
+            }
+            3 -> {
+                pengelolaKontak.tampilkanKontak()
+            }
+            4 -> {
+                println("Terima kasih!")
+                isRunning = false
+            }
+            else -> {
+                println("Pilihan tidak valid. Silakan pilih 1, 2, 3, atau 4.")
+            }
         }
-        scanner.close()
+    }
+    scanner.close()
 }

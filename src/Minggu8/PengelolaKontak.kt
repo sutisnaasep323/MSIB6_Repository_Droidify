1f2a816ab8c0cfa58173eb905d5af0edd52539a0
Riska Maulida <riskamaulida17@gmail.com>
2024-04-18 15:50:27 +0700

    Soal Visibility Control selesai

--- src/Minggu8/PengelolaKontak.kt
+++ src/Minggu8/PengelolaKontak.kt
@@ -3,21 +3,34 @@ package Minggu8
 import java.util.Scanner
 
 data class Kontak(
-        val name: String,
-        val phoneNumber: String,
-        val email: String
+       private val name: String, // Visibility control ditambahkan disini
+       private val phoneNumber: String, // Visibility control ditambahkan disini
+       private val email: String // Visibility control ditambahkan disini
 )
+{ //tambahkam getter method
+        fun getName(): String {
+                return name
+        }
 
+        fun getPhoneNumber(): String {
+                return phoneNumber
+        }
+
+        fun getEmail(): String {
+                return email
+        }
+}
 class PengelolaKontak {
         private val kontakList: MutableList<Kontak> = mutableListOf()
-
+        //Atribut kontakList tetap private karena ini merupakan struktur data internal dari kelas PengelolaKontak dan tidak seharusnya diakses langsung dari luar
+        // untuk (tambahKontak, hapusKontak, tampilkanKontak) metode publik karena memang seharusnya bisa diakses dari luar kelas tersebut.
         fun tambahKontak(kontak: Kontak) {
                 kontakList.add(kontak)
-                println("Kontak '${kontak.name}' berhasil ditambahkan.")
+                println("Kontak '${kontak.getName()}' berhasil ditambahkan.")
         }
 
         fun hapusKontak(namaKontak: String) {
-                val kontakDitemukan = kontakList.find { it.name == namaKontak }
+                val kontakDitemukan = kontakList.find { it.getName() == namaKontak }
                 if (kontakDitemukan != null) {
                         kontakList.remove(kontakDitemukan)
                         println("Kontak '$namaKontak' berhasil dihapus.")
@@ -32,11 +45,12 @@ class PengelolaKontak {
                 } else {
                         println("Daftar Kontak:")
                         kontakList.forEachIndexed { index, kontak ->
-                                println("${index + 1}. ${kontak.name}, ${kontak.phoneNumber}, ${kontak.email}")
+                                println("${index + 1}. ${kontak.getName()}, ${kontak.getPhoneNumber()}, ${kontak.getEmail()}")
                         }
                 }
         }
 }
+
 
 fun main() {
         val scanner = Scanner(System.`in`)

592c1651c6425201d23f77b9a3f8a279338a3de1
nova.hrt <novahertanti@gmail.com>
2024-05-03 13:27:04 +0700

    point B atau databinding selesai

--- src/Minggu11/app/src/main/java/com/asep/myapplication/MainActivity.kt
+++ src/Minggu11/app/src/main/java/com/asep/myapplication/MainActivity.kt
@@ -1,15 +1,19 @@
-package com.asep.myapplication
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.asep.myapplication.User
 import com.asep.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var user: User
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        user = User("John Doe", 30)
+
+        binding.user = user
     }
 }
